6c6a16f1db5a136efbd6383464e06ca09ac6b530
Patrick Nance <jpnance@gmail.com>
2022-10-21 13:29:37 -0700

    Calculating and styling the best/worst quotes.

--- src/QuoteTable.tsx
+++ src/QuoteTable.tsx
@@ -2,13 +2,43 @@ import { FC } from 'react';
 import { PartQuoteItem, QuoteItem } from './QuoteAnalyzer';
 import { PricingFieldSelectorValue } from './PricingFieldSelector';
 
+import styles from './QuoteTable.module.css';
+
 export interface QuoteTableProps {
 	partQuotes: PartQuoteItem[];
 	companyColumns: string[];
 	pricingFieldSelection: PricingFieldSelectorValue;
 };
 
+type BestWorstQuote = {
+	company: string | undefined;
+	price: number | undefined;
+};
+
 const QuoteTable: FC<QuoteTableProps> = ({ partQuotes, companyColumns, pricingFieldSelection }) => {
+	let bestPartQuotes = new Map<string, BestWorstQuote>();
+	let worstPartQuotes = new Map<string, BestWorstQuote>();
+
+	partQuotes.forEach((partQuote) => {
+		let bestPartQuote: BestWorstQuote = { company: undefined, price: undefined };
+		let worstPartQuote: BestWorstQuote = { company: undefined, price: undefined };
+
+		partQuote.Quotes.forEach((partQuoteQuote) => {
+			if (!bestPartQuote.price || partQuoteQuote[pricingFieldSelection] < bestPartQuote.price) {
+				bestPartQuote.company = partQuoteQuote.Company;
+				bestPartQuote.price = partQuoteQuote[pricingFieldSelection];
+			}
+
+			if (!worstPartQuote.price || partQuoteQuote[pricingFieldSelection] > worstPartQuote.price) {
+				worstPartQuote.company = partQuoteQuote.Company;
+				worstPartQuote.price = partQuoteQuote[pricingFieldSelection];
+			}
+		});
+
+		bestPartQuotes.set(partQuote.PartNo, bestPartQuote);
+		worstPartQuotes.set(partQuote.PartNo, worstPartQuote);
+	});
+
 	return (
 		<table>
 			<thead>
@@ -30,13 +60,27 @@ const QuoteTable: FC<QuoteTableProps> = ({ partQuotes, companyColumns, pricingFi
 						{companyColumns.map((companyColumn, i) => {
 							let quote = partQuote.Quotes.find((partQuoteQuote) => partQuoteQuote.Company === companyColumn);
 
+							let isBestQuote = false;
+							let isWorstQuote = false;
+
 							let quoteText = '';
 
 							if (quote) {
 								quoteText = `$${quote[pricingFieldSelection].toFixed(2)}`;
+
+								let bestPartQuote = bestPartQuotes.get(partQuote.PartNo);
+								let worstPartQuote = worstPartQuotes.get(partQuote.PartNo);
+
+								if (bestPartQuote?.company === companyColumn) {
+									isBestQuote = true;
+								}
+
+								if (worstPartQuote?.company === companyColumn) {
+									isWorstQuote = true;
+								}
 							}
 
-							return <td key={`quote-table-company-column-${i}`}>{quoteText}</td>
+							return <td key={`quote-table-company-column-${i}`} className={`${isBestQuote ? styles.best : null} ${isWorstQuote ? styles.worst : null}`}>{quoteText}</td>
 						})}
 					</tr>
 				))}
